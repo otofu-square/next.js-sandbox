198556215bac3431e91fea23cf6336896acaa367
otofu-square <yukita@feedforce.jp>
2017-04-23 17:10:17 +0900

    :+1: Add link for about page by next/link

--- next.js-tutorial/pages/index.js
+++ next.js-tutorial/pages/index.js
@@ -1,6 +1,12 @@
-const Index = () =>
+import Link from 'next/link'
+
+const Index = () => (
   <div>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
     <p>Hello Next.js</p>
   </div>
+)
 
 export default Index
