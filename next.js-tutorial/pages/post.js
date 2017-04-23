fa3faef109b9bbcc9a3d1f3e6e7e2708b47eb11a
otofu-square <yukita@feedforce.jp>
2017-04-23 23:57:02 +0900

    :tada: Finish tutorial

--- next.js-tutorial/pages/post.js
+++ next.js-tutorial/pages/post.js
@@ -1,12 +1,21 @@
 import Layout from '../components/MyLayout'
+import fetch from 'isomorphic-unfetch'
 
-const Content = (props) =>
-  <div>
-    <h1>{props.url.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-
-export default (props) =>
+const Post = (props) =>
   <Layout>
-    <Content url={props.url}/>
+    <h1>{props.movie.Title}</h1>
+    <p>{props.movie.Plot}</p>
+    <img src={props.movie.Poster}/>
   </Layout>
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query
+  const res = await fetch(`https://www.omdbapi.com/?i=${id}`)
+  const movie = await res.json()
+
+  console.log(`Fetched movie: ${movie.Title}`)
+
+  return { movie }
+}
+
+export default Post
