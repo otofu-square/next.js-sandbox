ff5f9ff1a7505d36c7966137b33a3ce388c24d20
otofu-square <yukita@feedforce.jp>
2017-04-23 22:15:18 +0900

    :recycle: Refactor post component

--- next.js-tutorial/pages/post.js
+++ next.js-tutorial/pages/post.js
@@ -1,7 +1,12 @@
 import Layout from '../components/MyLayout'
 
-export default (props) =>
-  <Layout>
+const Content = (props) =>
+  <div>
     <h1>{props.url.query.title}</h1>
     <p>This is the blog post content.</p>
+  </div>
+
+export default (props) =>
+  <Layout>
+    <Content url={props.url}/>
   </Layout>
