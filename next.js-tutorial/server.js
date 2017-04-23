7cc575ec3a24f720c4f93fab65411923094e18fa
otofu-square <yukita@feedforce.jp>
2017-04-23 22:47:43 +0900

    :bug: Fix some mistake

--- next.js-tutorial/server.js
+++ next.js-tutorial/server.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import next from 'next'
+const express = require('express')
+const next = require('next')
 
-const dev = process.env.NODE_ENV || 'production'
-const app = next({ next })
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
